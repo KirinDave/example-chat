2f6d8d656d911576b08e6615034023a4c8598af8
Dave Fayram <dfayram@gmail.com>
2009-12-06 17:24:53 -0800

    Reduced code redundancy.

--- src/exchat.clj
+++ src/exchat.clj
@@ -25,47 +25,47 @@
 ;;; It uses bindings for clarity.
 
 (def *username* "Someone")
-(defn- join-room [roomname] 
-  (let [room (make-get-room! roomname)]
-    (send room agent-add-user-room *username* *out*)
-    (send room agent-write-room (str ":: " *username* " has joined the room."))))
-(defn- leave-room [roomname]
-  (let [room (make-get-room! roomname)]
-    (send room agent-remove-user-room *username*)
-    (send room agent-write-room (str ":: " *username* " has left the room."))))
-(defn- say [roomname message]
-  (let [room (make-get-room! roomname)]
-    (send room agent-write-room (str *username* ": " message))))
+(defn- join-room [room] 
+  (send room agent-add-user-room *username* *out*)
+  (send room agent-write-room (str ":: " *username* " has joined the room.")))
+(defn- leave-room [room]
+  (send room agent-remove-user-room *username*)
+  (send room agent-write-room (str ":: " *username* " has left the room.")))
+(defn- say [room message]
+  (send room agent-write-room (str *username* ": " message)))
 (defn- display-users [roomname]
   (println "--- Users in" roomname)
   (doseq [user @(make-get-room! roomname)] (println "* " (user 0))))
 (defn- cleanup []
   (doseq [room (vals @rooms)]
-      (when-first [active-room (filter #(= *username* (% 0)) @room)]
+      (when-first [active-room (filter #(= *out* (% 1)) @room)]
                   (send room agent-write-room (str *username* " has disconnected.")))
       (send room agent-remove-user-room *username*)))
+
+;;; Main and client-loop do the heavy lifting of making a server
+;;; and running it. 
 
 (defn- client-loop [in out]
   (binding [*in* (reader in)
             *out* (writer out)]
     (print "\nWhat is your name? ") (flush)
     (binding [*username* (read-line)]
-      (join-room "default")
+      (join-room (make-get-room! "default"))
       (try (loop [input (read-line) roomname "default"]
              (when input 
                (let [splitform (re-split #" " input)
                      cmd (first splitform)
-                     rest-str (apply str (rest splitform))]
-                 (binding [*out* *err*] (println "Reading a line for" *username*))
+                     rest-str (apply str (rest splitform))
+                     room (make-get-room! roomname)]
                  (cond
-                  (= cmd "/join") (do (leave-room roomname) 
-                                      (join-room rest-str) 
+                  (= cmd "/join") (do (leave-room room) 
+                                      (join-room (make-get-room! rest-str)) 
                                       (flush)
                                       (recur (read-line) rest-str))
-                  (= cmd "/who")  (do (display-users roomname) (flush)
+                  (= cmd "/who")  (do (display-users room) (flush)
                                       (recur (read-line) roomname))
                   (= cmd "/quit") :done
-                  true            (do (say roomname input) (flush)
+                  true            (do (when (not (empty? input)) (say room input) (flush))
                                       (recur (read-line) roomname))))))
            (finally (cleanup))))))
  
